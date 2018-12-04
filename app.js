65d1541387c4cd986828cbe48544e30a279e8b92
Adriana <s.aguirre.adriana@gmail.com>
2018-12-03 21:56:07 -0600

    Modificaciones al estilo

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var uno = document.querySelector(".text1");
 var dos = document.querySelector(".text2");
 var tres = document.querySelector(".text3");
 var username = document.querySelector(".username");
+var icono = document.querySelector(".fa-user-alt")
 
 
 //Variables Condición
@@ -21,6 +22,10 @@ setTimeout(function() {
 
 setTimeout(function() {
   tres.style.display = "block";
+}, 5000);
+
+setTimeout(function() {
+  icono.style.display = "block";
 }, 6000);
 
 setTimeout(function() {
@@ -30,21 +35,30 @@ setTimeout(function() {
     var key = e.which || e.keyCode;
     if (key === 13) { 
     	if (username.value.length <= 2){
+    		usernameCondition.style.display = "block";
     		usernameCondition.textContent = "Es necesario ingresar un nombre de usuario válido";
-    		answer.textContent = "";
-    	} else if (username.value.length >= 6){
-    		
-    		
+    	} else if (username.value.length >= 6){	
+    		usernameCondition.style.display = "block";
     		usernameCondition.textContent = "That's a long name.";
-    		answer.textContent = "Nice to meet you, " + username.value;
+    		setTimeout(function(){
+    			answer.style.display = "block";
+  				answer.textContent = "Nice to meet you, " + username.value;
+  				}, 1000);
+    		
     	} else{
-    	
-    
+    		usernameCondition.style.display = "block";
     		usernameCondition.textContent ="That's a short name.";
-    		answer.textContent = "Nice to meet you, " + username.value;
+    		setTimeout(function(){
+    			answer.style.display = "block";
+  				answer.textContent = "Nice to meet you, " + username.value;
+  				}, 1000);
+    		
     	}
     }
+    
 });
+
+  
 
 }, 8000);
 
